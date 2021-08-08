56d4d6d5d404dee8c0057d71b8a39b67a3b59b00
LukaHarambasic <luka@harambasic.de>
2021-08-08 21:56:39 +0200

    working setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+
+// TODO: sample for global components
+// import Butt from './components/Butt.vue'
+// app.component('Butt', Butt)
+
+app.mount('#app')
